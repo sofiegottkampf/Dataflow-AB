39702862b075221b73e6cbff3c7dace3dd261b9f
sofiegottkampf <146066553+sofiegottkampf@users.noreply.github.com>
2023-10-09 16:00:34 +0200

    Fixat i javascript + annat
    tagit bort "ej ska ha"- filer, fixat i javascript filen

--- Dataflow.js
+++ Dataflow.js
@@ -1,5 +1,34 @@
 
 function myFunction() {
-  document.body.style.backgroundColor = "white";
+  document.body.style.backgroundColor = "pink";
 }
+
+function updateClock() {
+  let date = new Date ();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+let meridiem = "AM";
+if (hours <'>12')    {
+hours -= 12;
+meridiem = "PM";
+}
+if (hours === 0) {
+hours = 12;
+}
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+if (seconds <10) {
+seconds = "0" + seconds;
+}
+let time = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+document.getElementById("clockDisplay").innerHTML = time;
+}
+setInterval(updateClock, 1000);
+const main=()=>{
+ 
+}
+
+
 
