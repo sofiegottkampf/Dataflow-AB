6ad022abab9859e642b6783b804ade2fe33a3c2c
sofiegottkampf <146066553+sofiegottkampf@users.noreply.github.com>
2023-10-09 13:21:02 +0200

    testning (ska ej användas)
    OBS! Ett error uppstod i Github, behövde då commita om filerna. Men denna ska skrivas över!

--- Dataflow.js
+++ Dataflow.js
@@ -1,16 +1,5 @@
 
-//Skapa en funktion som gör två saker: ändrar färg och ändrar text.
-function clickFunction(){
-    //Hämta våran p tagg och spara den i en variabel
-let ourText = document.querySelector("p");
-// Hämta våran Div via dens IDoch sparar den i en variabel
-let ourDiv = document.querySelector("#color-changer");    
-
-// Ändra bakgrundsfärgen till ourDiv till grön
-ourDiv.style.backgroundColor = "green";
-// Ändra text på ourText till "jag har ändrats!"
-ourText.innerText = "Jag har ändrats!";
-//let ourText.innerText = ourText.innerText
-}
-// Koppla denna funktion till onclick
+function myFunction() {
+    document.getElementById("myDIV").style.backgroundColor = "lightblue";
+  }
 
