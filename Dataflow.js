4caa496e864cdb90f66f1d74e2bc0363371b02c5
sofiegottkampf <146066553+sofiegottkampf@users.noreply.github.com>
2023-10-10 14:34:38 +0200

    Klickbutton

--- Dataflow.js
+++ Dataflow.js
@@ -1,34 +1,36 @@
 
 function myFunction() {
-  document.body.style.backgroundColor = "pink";
+  document.body.style.backgroundColor = "lightblue";
 }
 
 function updateClock() {
-  let date = new Date ();
+  let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-let meridiem = "AM";
-if (hours <'>12')    {
-hours -= 12;
-meridiem = "PM";
-}
-if (hours === 0) {
-hours = 12;
-}
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-if (seconds <10) {
-seconds = "0" + seconds;
-}
-let time = hours + ":" + minutes + ":" + seconds + " " + meridiem;
-document.getElementById("clockDisplay").innerHTML = time;
+  let meridiem = "AM";
+  if (hours < '>12') {
+    hours -= 12;
+    meridiem = "PM";
+  }
+  if (hours === 0) {
+    hours = 12;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  let time = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+  document.getElementById("clockDisplay").innerHTML = time;
 }
 setInterval(updateClock, 1000);
-const main=()=>{
- 
+const main = () => {
+
 }
+const contactButton = document.getElementById("contactButton");
+
 
 
 
